7c39e8e8298bec661c6e5b0c6e1c9d74a2c97979
Justin Voelkel <justin@cinpleweb.com>
2015-03-09 12:04:33 -0400

    Added master layout view and updated index view to reflect change

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    @yield('meta')
+
+    <title>Laravel and Angular Together</title>
+</head>
+<body>
+
+<div class="container">
+
+    @yield('content')
+
+</div><!--container-->
+
+</body>
+<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+</html>
